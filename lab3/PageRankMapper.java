5df76ffacfb0799913e7953be7762ff2310798cc
Huixuan Wang <whx987172@gmail.com>
2022-10-06 15:00:56 -0400

    first commit on lab3

--- lab3/PageRankMapper.java
+++ lab3/PageRankMapper.java
@@ -9,45 +9,6 @@ import org.apache.hadoop.io.MapWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-public class TextDoublePair implements Writable {
-  private Text source;
-  private double probability;
-
-  public TextDoublePair(){
-    set(new Text(), -1);
-  }
-  public TextDoublePair(String first, double second){
-    set(new Text(first), second);
-  }
-  public TextDoublePair(Text first, double second){
-    set(first, second);
-  }
-  public void set(Text first, double second) {
-    this.source = first;
-    this.probability = second;
-  }
-  public Text getSource() {
-    return source;
-  }
-  public double getPr() {
-    return probability;
-  }
-
-  @Override
-  public void write(DataOutput out) throws IOException {
-    source.write(out);
-    out.writeDouble(probability);
-  }
-  @Override
-  public void readFields(DataInput in) throws IOException {
-    source.readFields(in);
-    probability = in.readDouble();
-  }
-  @Override
-  public String toString() {
-    return source + "\t" + probability;
-  }
-}
 
 public class PageRankMapper 
   extends Mapper<LongWritable, Text, Text, TextDoublePair> {

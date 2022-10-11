2bf8545dc851f9778889ff0643ce68d7d3ed705f
Huixuan Wang <whx987172@gmail.com>
2022-10-10 22:38:03 -0400

    Update 3PageRank.sh

--- lab3/3PageRank.sh
+++ lab3/3PageRank.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 echo Run Page Rank Algorithm for 3 times!
 if [ $# != 2 ] ; then
-echo $#
 echo "USAGE: bash 3PageRank.sh input_file output_directory"
 exit 1;
 fi
 
-input=$1
 hadoop fs -rm -r -f $2
 hadoop fs -rm -r -f "temp1"
 
-hadoop jar pageRank.jar PageRank $input temp1
+hadoop jar pageRank.jar PageRank $1 temp1
 echo finish 1st time!
 hadoop fs -rm -r -f "temp2"
 hadoop jar pageRank.jar PageRank temp1/part-r-00000 temp2
